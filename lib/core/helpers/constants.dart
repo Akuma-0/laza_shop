ce285a4b59662b4a4e3c70d44bd855a5964c739c
Hany <Hanym7md90@gmail.com>
2025-11-23 04:40:37 +0200

    feat: Fix refresh token expiration handling and improve UI consistency

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
+
 bool isLoggedInUser = false;
 bool isOnboardingSeen = false;
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class SharedPrefKeys {
   static const String userToken = 'userToken';
--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:laza_shop/core/helpers/extensions.dart';
 import 'package:laza_shop/core/networking/api_services.dart';
+import 'package:laza_shop/core/routing/routes.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import '../helpers/constants.dart';
 import '../helpers/shared_pref_helper.dart';
@@ -49,40 +51,84 @@ class DioFactory {
       InterceptorsWrapper(
         onError: (error, handler) async {
           if (error.response?.statusCode == 401) {
-            String? refreshToken = await SharedPrefHelper.getSecuredString(
+            final refreshToken = await SharedPrefHelper.getSecuredString(
               SharedPrefKeys.refreshToken,
             );
-            if (refreshToken != null) {
-              ApiService apiService = ApiService(DioFactory.getDio());
-              try {
-                final response = await apiService.refreshToken({
-                  'refreshToken': refreshToken,
-                });
-                String newToken = response.accessToken!;
-                SharedPrefHelper.setSecuredString(
+            if (refreshToken.isNullOrEmpty()) {
+              // No refresh token available, navigate to login
+              await SharedPrefHelper.removeSecuredString(
+                SharedPrefKeys.userToken,
+              );
+              isLoggedInUser = false;
+
+              navigatorKey.currentState?.pushNamedAndRemoveUntil(
+                Routes.loginScreen,
+                (route) => false,
+              );
+
+              return handler.reject(
+                DioException(
+                  requestOptions: error.requestOptions,
+                  error: 'Session expired. Please login again.',
+                  type: DioExceptionType.badResponse,
+                  response: error.response,
+                ),
+              );
+            }
+            final apiService = ApiService(DioFactory.getDio());
+            try {
+              final response = await apiService.refreshToken({
+                'refreshToken': refreshToken,
+              });
+              final newToken = response.accessToken!;
+
+              await SharedPrefHelper.setSecuredString(
+                SharedPrefKeys.userToken,
+                newToken,
+              );
+              await SharedPrefHelper.setSecuredString(
+                SharedPrefKeys.refreshToken,
+                response.refreshToken!,
+              );
+              DioFactory.setTokenIntoHeaderAfterLogin(newToken);
+              // Retry the original request
+              final options = error.requestOptions;
+              final cloneReq = await DioFactory.getDio().request(
+                options.path,
+                options: Options(
+                  method: options.method,
+                  headers: options.headers,
+                ),
+                data: options.data,
+                queryParameters: options.queryParameters,
+              );
+              return handler.resolve(cloneReq);
+            } catch (e) {
+              // Refresh token failed (expired or invalid)
+              if (e is DioException && e.response?.statusCode == 400) {
+                await SharedPrefHelper.removeSecuredString(
                   SharedPrefKeys.userToken,
-                  newToken,
                 );
-                SharedPrefHelper.setSecuredString(
+                await SharedPrefHelper.removeSecuredString(
                   SharedPrefKeys.refreshToken,
-                  response.refreshToken!,
                 );
-                DioFactory.setTokenIntoHeaderAfterLogin(newToken);
-                final options = error.requestOptions;
-                final cloneReq = await DioFactory.getDio().request(
-                  options.path,
-                  options: Options(
-                    method: options.method,
-                    headers: options.headers,
+                isLoggedInUser = false;
+
+                // Navigate to login screen
+                navigatorKey.currentState?.pushNamedAndRemoveUntil(
+                  Routes.loginScreen,
+                  (route) => false,
+                );
+
+                return handler.reject(
+                  DioException(
+                    requestOptions: error.requestOptions,
+                    error: 'Session expired. Please login again.',
+                    type: DioExceptionType.badResponse,
+                    response: e.response,
                   ),
-                  data: options.data,
-                  queryParameters: options.queryParameters,
                 );
-                return handler.resolve(cloneReq);
-              } catch (e) {
-                return handler.next(error);
               }
-            } else {
               return handler.next(error);
             }
           } else {
