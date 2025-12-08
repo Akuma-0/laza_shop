af85b3b5c788995302aeeb9ead5079d2d8c904d2
Hany <Hanym7md90@gmail.com>
2025-12-08 18:04:18 +0200

    feat: Enhance cart functionality with loading states and item deletion support

--- lib/features/home/ui/widgets/categories/category_bloc_builder.dart
+++ lib/features/home/ui/widgets/categories/category_bloc_builder.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:laza_shop/features/home/data/models/categories_response_model.dart';
 import 'package:laza_shop/features/home/ui/widgets/categories/category_list_view.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import '../../../../../core/themes/text_styles.dart';
 import '../../../logic/home_cubit.dart';
 import '../../../logic/home_state.dart';
+import 'category_card.dart';
 
 class CategoryBlocBuilder extends StatelessWidget {
   const CategoryBlocBuilder({super.key});
@@ -17,8 +21,17 @@ class CategoryBlocBuilder extends StatelessWidget {
           current is CategoriesError,
       builder: (context, state) {
         return state.maybeWhen(
-          categoriesLoading: () =>
-              const Center(child: CircularProgressIndicator()),
+          categoriesLoading: () => Skeletonizer(
+            enabled: true,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(10, (index) {
+                return CategoryCard(
+                  category: Category(id: '', name: '', imageUrl: ''),
+                );
+              }),
+            ),
+          ),
           categoriesSuccess: (categoriesList) {
             return setupSuccess(categoriesList);
           },
@@ -39,17 +52,7 @@ class CategoryBlocBuilder extends StatelessWidget {
         children: [
           Icon(Icons.error_outline, size: 20.sp, color: Colors.red),
           SizedBox(height: 16.h),
-          Text(
-            'Failed to load categories',
-            style: TextStyle(fontSize: 16, color: Colors.red),
-          ),
-          SizedBox(height: 8.h),
-          Text(
-            'Please check your internet connection',
-            style: TextStyle(fontSize: 14, color: Colors.grey),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
+          Text('Failed to load categories', style: TextStyles.font13W600),
         ],
       ),
     );
